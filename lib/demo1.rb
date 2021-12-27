1ee7df50d8ace45395733ebd619dcce38f3e291e
David Vargas <dvarrui@protonmail.com>
2021-12-27 20:44:18 +0000

    N demos

---
+++ lib/demo1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "gtk3"
+
+builder_file = "#{File.expand_path(File.dirname(__FILE__))}/../glade/asker-gui.glade"
+
+# Construct a Gtk::Builder instance and load our UI description
+builder = Gtk::Builder.new(:file => builder_file)
+
+# Connect signal handlers to the constructed widgets
+window = builder.get_object("window")
+window.signal_connect("destroy") { Gtk.main_quit }
+
+#button = builder.get_object("btn_quit")
+#button.signal_connect("clicked") { Gtk.main_quit }
+
+#button = builder.get_object("button2")
+#button.signal_connect("clicked") { puts "Hello World" }
+
+#button = builder.get_object("quit")
+#button.signal_connect("clicked") { Gtk.main_quit }
+
+window.show_all
+Gtk.main
