dc8ee58b738f2491e857b4ac2b424cccc8dfefb7
David Vargas <dvarrui@protonmail.com>
2022-08-10 11:54:28 +0100

    [U] .

--- lib/asker/gui/window.rb
+++ lib/asker/gui/window.rb
@@ -12,35 +12,41 @@ module Asker
       end
 
       def create_form
-        form {
-          stretchy false
-          entry {
-            label 'Filepath'
-            text <=> [self, :filepath]
+        vertical_box {
+          horizontal_box {
+            label 'Concept'
+            button('Salir') {
+              stretchy true
+              on_clicked do
+                msg_box('You entered', @filepath)
+              end
+            }
+            button("Add")
+          }
+
+          form {
+            stretchy false
+            entry {
+              label 'Name'
+              text <=> [self, :filepath]
+            }
+            entry {
+              label 'Tags'
+              text <=> [self, :filepath]
+            }
           }
         }
       end
 
-      def create_button
-        button('Button') {
-          stretchy false
-          on_clicked do
-            msg_box('You entered', @filepath)
-          end
-        }
-      end
-
       def launch
-        window('Asker GUI') { |w|
-          # margined true
-
-          #vertical_box {
-            horizontal_box {
+        window('Asker GUI', 500, 300) { |w|
+          tab {
+            tab_item("Maps") {}
+            tab_item("Concepts") {}
+            tab_item("Concept#name") {
               create_form
-              create_button
             }
-            #non_wrapping_multiline_entry
-          #}
+          }
 
           on_closing do
             puts 'Bye Bye'
